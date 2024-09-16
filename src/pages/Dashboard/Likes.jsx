811d44640d2c529d920324b0b9382c4716c054c4
master <https://github.com/Azamatjon-developer/exam5-medium.git>
2024-09-16 18:37:21 +0500

    LikedHeartCoundDone

--- src/pages/Dashboard/Likes.jsx
+++ src/pages/Dashboard/Likes.jsx
@@ -2,26 +2,103 @@ import React from 'react'
 import Heart from '../../assets/Images/LikedHeart.png'
 import User from '../../assets/Images/LikesUser.svg'
 import PlaylistLike from '../../components/playlistLike'
+import { useSelector } from 'react-redux'
+import { ClockIcon } from '../../assets/Images/Icons'
+import { FaHeart } from 'react-icons/fa'
+
 function Likes() {
+  const wishlist = useSelector((s) => s.heart.value)
+  console.log(wishlist)
   return (
-    <div id="likes" className="col-span-8">
-      <div className="pt-[28px] pl-[42px] pb-[31px] pr-[32px]">
-        <div className="flex  items-center text-white mb-[32px]">
-          <img src={Heart} alt="HeartImage" width={'297px'} height={'297px'} />
-          <div className="">
-            <p className="font-medium text-[16px]">
-              PUBLIC <br /> PLAYLIST
+    <div
+      id="likes"
+      className="col-span-8 h-screen overflow-y-auto bg-gradient-to-b from-[#181818] to-[#121212] text-white p-[30px]"
+    >
+      {/* Header Section */}
+      <div className="flex items-center gap-10 mb-[32px]">
+        <img
+          src={Heart}
+          alt="HeartImage"
+          className="w-[297px] h-[297px] rounded-lg shadow-2xl hover:scale-105 transition-transform duration-300 ease-in-out"
+        />
+        <div>
+          <p className="text-xs tracking-wide text-gray-300 mb-2 uppercase">
+            Public <br /> Playlist
+          </p>
+          <h2 className="text-6xl font-extrabold text-white mb-2 hover:text-[#1DB954] transition-colors duration-300">
+            Liked Songs
+          </h2>
+          <div className="flex items-center gap-4 mt-[12px]">
+            <img
+              src={User}
+              alt="userImage"
+              className="w-[30px] h-[30px] rounded-full border-2 border-[#1DB954]"
+            />
+            <p className="text-lg text-white font-semibold">
+              davedirect3 <span className="text-gray-400">34 songs</span>
             </p>
-            <h2 className="font-bold text-[89px]">Liked Songs</h2>
-            <div className="flex items-center gap-[5px] mt-[12px]">
-              <img src={User} alt="userImage" width={'30px'} height={'30px'} />
-              <p>
-                davedirect3 <span>34 songs</span>{' '}
-              </p>
-            </div>
           </div>
         </div>
-        <PlaylistLike />
+      </div>
+
+      <PlaylistLike />
+
+      {/* Table Headers */}
+      <div className="mt-8 grid grid-cols-12 gap-4 text-[#B3B3B3] text-sm border-b border-gray-900 pb-2 uppercase font-semibold">
+        <div className="col-span-1">#</div>
+        <div className="col-span-5">Title</div>
+        <div className="col-span-3">Album</div>
+        <div className="col-span-2">Date Added</div>
+        <div className="col-span-1 flex justify-end text-right">
+          <ClockIcon className="w-5 h-5 mx-auto" />
+        </div>
+      </div>
+
+      {/* Song List */}
+      <div className="">
+        {wishlist?.length > 0 ? (
+          wishlist.map((item, index) => (
+            <div
+              key={item.id}
+              className="grid grid-cols-12 gap-4 items-center py-4 hover:bg-[#1e4552] transition-all duration-300 ease-in-out rounded-xl px-2"
+            >
+              {/* Song Number */}
+              <div className="col-span-1 text-gray-400">{index + 1}</div>
+
+              {/* Song Info */}
+              <div className="col-span-5 flex items-center gap-4">
+                <img
+                  src={item.album.images[1]?.url || item.album.images[0]?.url}
+                  alt={item.name}
+                  className="w-[55px] h-[55px] rounded-xl shadow-md hover:scale-110 transition-transform duration-300 ease-in-out"
+                />
+                <div>
+                  <p className="text-white font-semibold">
+                    {item.name}
+                  </p>
+                  <p className="text-sm text-gray-400">
+                    {item.artists[0]?.name}
+                  </p>
+                </div>
+              </div>
+
+              {/* Album Info */}
+              <div className="col-span-3 text-gray-400">{item.album.name}</div>
+
+              {/* Date Added */}
+              <div className="col-span-2  text-white">{item.album.release_date}</div>
+
+              {/* Like Icon */}
+              <div className="col-span-1 text-right flex items-center justify-end gap-2 text-center">
+                <FaHeart className="text-[#63CF6C] hover:text-red-500 transition-colors duration-300 cursor-pointer scale-125" />
+              </div>
+            </div>
+          ))
+        ) : (
+          <p className="text-center col-span-12 text-gray-400">
+            No liked songs yet.
+          </p>
+        )}
       </div>
     </div>
   )
--- src/pages/Dashboard/Playlist.jsx
+++ src/pages/Dashboard/Playlist.jsx
@@ -1,26 +1,33 @@
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import SpotifyWebApi from 'spotify-web-api-node';
-import PlaylistLike from '../../components/playlistLike';
-import ActiveIcon from '../../assets/Images/PlaylistActive.svg';
-import { CgLock } from 'react-icons/cg';
-import { ClockIcon } from '../../assets/Images/Icons';
+import { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import SpotifyWebApi from 'spotify-web-api-node'
+import PlaylistLike from '../../components/playlistLike'
+import { ClockIcon } from '../../assets/Images/Icons'
+import { CiHeart } from 'react-icons/ci'
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleEvent } from '../../Redux/Wishlist'
+import { FaHeart } from "react-icons/fa";
 
 function Playlist() {
-  const location = useLocation();
-  const spotifyApi = new SpotifyWebApi();
-  const track = location.state?.track;
-  const [artistMusics, setArtistMusic] = useState([]);
-
+  const location = useLocation()
+  const spotifyApi = new SpotifyWebApi()
+  const track = location.state?.track
+  const [artistMusics, setArtistMusic] = useState([])
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const wishlist = useSelector(s => s.heart.value)
   useEffect(() => {
-    if (!track) return;
+    if (!track) return
     spotifyApi
       .searchTracks(track.artistName)
-      .then((res) => setArtistMusic(res.body.tracks.items));
-  }, [track]);
+      .then((res) => setArtistMusic(res.body.tracks.items))
+  }, [track])
 
   return (
-    <section id="playlist" className="col-span-8 h-screen overflow-y-auto p-[30px] bg-[#121212] text-white">
+    <section
+      id="playlist"
+      className="col-span-8 h-screen overflow-y-auto p-[30px] bg-[#121212] text-white"
+    >
       {track ? (
         <div className="pb-[62px]">
           <div className="flex items-center gap-10 mb-10">
@@ -30,10 +37,16 @@ function Playlist() {
               className="w-[297px] h-[297px] rounded-lg shadow-lg"
             />
             <div>
-              <p className="text-sm tracking-tight text-gray-400 mb-2">PUBLIC PLAYLIST</p>
-              <h2 className="text-5xl font-bold text-white mb-2">{track.trackName}</h2>
+              <p className="text-sm tracking-tight text-gray-400 mb-2">
+                PUBLIC PLAYLIST
+              </p>
+              <h2 className="text-5xl font-bold text-white mb-2">
+                {track.trackName}
+              </h2>
               <p className="text-xl text-white mb-2">{track.artistName}</p>
-              <p className="text-lg text-gray-400">Julia Wolf, ayokay, Khalid, and more</p>
+              <p className="text-lg text-gray-400">
+                Julia Wolf, ayokay, Khalid, and more
+              </p>
               <p className="text-sm text-gray-500 pt-[12px]">
                 Made for <b className="text-white">davedirect3</b>
                 <span className="ml-2">34 songs, 2hr 01 min</span>
@@ -54,9 +67,11 @@ function Playlist() {
 
             {artistMusics.length > 0 ? (
               artistMusics.map((item, index) => (
-                <div key={item.id} className="contents hover:bg-[#1f1f1f] transition-colors duration-200 py-2">
+                <div
+                  key={item.id}
+                  className="contents hover:bg-[#1f1f1f] transition-colors duration-200 py-5"
+                >
                   <div className="col-span-1 text-gray-400">{index + 1}</div>
-                  
                   <div className="col-span-5 flex items-center gap-4 text-white">
                     <img
                       src={item.album.images[0]?.url}
@@ -65,33 +80,42 @@ function Playlist() {
                     />
                     <div>
                       <p className="font-medium">{item.name}</p>
-                      <p className="text-sm text-gray-400">{item.artists[0]?.name}</p>
+                      <p className="text-sm text-gray-400">
+                        {item.artists[0]?.name}
+                      </p>
                     </div>
                   </div>
 
-                  <div className="col-span-3 text-gray-400">{item.album.name}</div>
+                  <div className="col-span-3 text-gray-400">
+                    {item.album.name}
+                  </div>
 
                   <div className="col-span-2 text-gray-400">3:25</div>
 
-                  <div className="col-span-1 text-right">
-                    <img
-                      src={ActiveIcon}
-                      alt="Like"
-                      className="w-5 h-5 mx-auto"
-                    />
+                  <div className="col-span-1 text-right" onClick={() =>dispatch(toggleEvent(item))}> 
+                    {
+                      wishlist?.some(s => s.id === item.id) ?    <FaHeart className="scale-150 text-[#63CF6C] " /> 
+                      :
+                      <CiHeart  className="scale-150 " />
+                    }
+                   
                   </div>
                 </div>
               ))
             ) : (
-              <p className="col-span-12 text-center text-gray-400 mt-6">No tracks available.</p>
+              <p className="col-span-12 text-center text-gray-400 mt-6">
+                No tracks available.
+              </p>
             )}
           </div>
         </div>
       ) : (
-        <p className="text-center text-xl text-gray-400 mt-6">No track data available</p>
+        <p className="text-center text-xl text-gray-400 mt-6">
+          No track data available
+        </p>
       )}
     </section>
-  );
+  )
 }
 
-export default Playlist;
+export default Playlist
